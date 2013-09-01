db198d830f72353532b7eb28e42d683a765fd73a
Alec Thilenius <Alec@Thilenius.com>
2013-09-01 11:06:18 -0600

    Created an "Engine" and "Editor" sub project.

---
+++ VoxelCraftOSX/Unmanaged/Editor/Editor/Editor/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Editor
+//
+//  Created by Alec Thilenius on 9/1/13.
+//  Copyright (c) 2013 Alec Thilenius. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ VoxelCraftOSX/Unmanaged/Editor/Editor/Editor/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Editor
+//
+//  Created by Alec Thilenius on 9/1/13.
+//  Copyright (c) 2013 Alec Thilenius. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
