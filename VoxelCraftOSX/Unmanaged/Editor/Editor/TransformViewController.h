b5414b294dd61b3f95b30196a2acbb4a6d05bb4a
Alec Thilenius <Alec@Thilenius.com>
2013-09-07 17:39:58 -0600

    About to delete something to see if it help performance as per: http://stackoverflow.com/questions/6355667/xcode-4-slow-performance

--- VoxelCraftOSX/Unmanaged/Editor/Editor/TransformViewController.h
+++ VoxelCraftOSX/Unmanaged/Editor/Editor/TransformViewController.h
@@ -10,7 +10,17 @@
 
 @interface TransformViewController : NSViewController
 {
+    IBOutlet NSTextField* PositionXField;
+    IBOutlet NSTextField* PositionYField;
+    IBOutlet NSTextField* PositionZField;
     
+    IBOutlet NSTextField* RotationXField;
+    IBOutlet NSTextField* RotationYField;
+    IBOutlet NSTextField* RotationZField;
+    
+    IBOutlet NSTextField* ScaleXField;
+    IBOutlet NSTextField* ScaleYField;
+    IBOutlet NSTextField* ScaleZField;
 }
 
 @end
---
+++ VoxelCraftOSX/Unmanaged/Engine/VCInspectorWindow.cpp
@@ -0,0 +1,19 @@
+//
+//  VCInspectorWindow.cpp
+//  Engine
+//
+//  Created by Alec Thilenius on 9/7/13.
+//  Copyright (c) 2013 Thilenius. All rights reserved.
+//
+
+#include "VCInspectorWindow.h"
+
+VCInspectorWindow::VCInspectorWindow()
+{
+    
+}
+
+VCInspectorWindow::~VCInspectorWindow()
+{
+    
+}
